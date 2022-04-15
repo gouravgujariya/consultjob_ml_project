4cd385499889288f84b3234191ea52e84168a432
Gourav gujariya <76390851+gouravgujariya@users.noreply.github.com>
2022-04-15 23:23:43 +0530

    Update consultjob1.py

--- consultjob1.py
+++ consultjob1.py
@@ -1,50 +1,66 @@
-%%writefile app.py
-import streamlit as st 
 import numpy as np
-import pandas as pd
-import random
-import pickle
-from pathlib import Path
+import streamlit as st
+import joblib
+from pipelin import predictors,bow_vector,spacy_tokenizer
+# filename = 'finalized_model.sav'
+# loaded_model = joblib.load(filename)
 
-filename1 = Path("/content/trained_lr_model.sav")
-filename2 = Path("/content/trained_model.sav")
-filename3 = Path("/content/trained_rf_model.sav")
-loaded_model = pickle.load(open(filename1, 'rb'))
-loaded_lr_model = pickle.load(open(filename2, 'rb'))
-loaded_rf_model = pickle.load(open(filename3, 'rb'))
 
-# prediction = loaded_model.predict(X_test[[1]])
-# print(prediction)
+def main():
+    # giving a title
+    st.title('job fraud detection')
 
-def get_classifier(clf_name,jobid,all_about_job):
-    clf = None
-    if clf_name == 'SVC':
-        st.write('SVC Accuracy: 0.98')
-        predicted_svc = loaded_model.predict([[jobid,all_about_job]])
-        print(predicted_svc)
-        return predicted_svc
-    elif clf_name == 'LogisticRegression':
-        st.write('lr Accuracy: 0.98')  
-        predicted_lr = loaded_model.predict([[jobid,all_about_job]])
-        print(predicted_lr)
-        return predicted_lr  
+    # getting the input data from the user
+    # input_data = (
+    # 'job_id', 'title', 'location', 'department', 'salary_range', 'company_profile', 'description', 'requirements',
+    # 'benefits', 'telecommuting', 'has_company_logo', 'has_questions', 'employment_type', 'required_experience',
+    # 'required_education', 'industry', 'function', 'fraudulent')
+
+    job_id = st.text_input('Number of job id')
+    title = st.text_input('title of job')
+    location = st.text_input('location of job')
+    department = st.text_input('department of job')
+    salary_range = st.text_input('salary range of job')
+    company_profile = st.text_input('company profile')
+    description = st.text_input('description')
+    requirements = st.text_input('requirements for job')
+    benefits = st.text_input('benefits')
+    telecommuting = st.text_input('telecommuting')
+    has_company_logo= st.text_input('do company have a logo 0,1')
+    has_questions = st.text_input('has questions 0,1')
+    employment_type = st.text_input('employment type eg internship..')
+    required_experience = st.text_input('required experience 0,1')
+    required_education = st.text_input('required education level')
+    industry = st.text_input('industry')
+    function = st.text_input('function')
+
+    # code for Prediction
+    job = ''
+
+    # creating a button for Prediction
+
+    if st.button('prediction results are:'):
+        job = job_pri(
+            ['job_id', 'title', 'location', 'department', 'salary_range', 'company_profile', 'description', 'requirements',
+    'benefits', 'telecommuting', 'has_company_logo', 'has_questions', 'employment_type', 'required_experience',
+    'required_education', 'industry', 'function'])
+
+    st.success(job)
+
+# filename = 'C:/Users/ergou/Documents/PythonScripts/consultjob1_1/finalized_model.sav'
+# loaded_model = joblib.load(filename)
+
+def job_pri(input_data):
+    input_data_as_array = np.asarray(input_data)
+
+    prediction = loaded_model.predict(input_data_as_array)
+    print(prediction)
+    if (prediction[0] == 0):
+      return 'job is not fraud'
     else:
-        prediction_rf = loaded_model.predict([[jobid,all_about_job]])
-        print(prediction_rf)
-        st.write('SVC Accuracy: 0.98')
-        return prediction_rf
+      return 'job is fraud'
 
-
-if __name__=='__main__':
-    st.title("hello streamlit")
-    st.write("""#explore diff clasifier
-    which one is best""")
-    classifier_name = st.sidebar.selectbox("select classifier",(" LogisticRegression"," RandomForestClassifier"," SVC"))
-    
-    # this is usefuull for manything
-    jobid = st.text_input(" job", "Type Here")
-    all_about_job = st.text_input("proper description of job", "Type Here")
-    if st.button("Predict"):
-        predicted = get_classifier(classifier_name,jobid,all_about_job)
-        st.success('The output is {}'.format(predicted))
-
+if __name__ == '__main__':
+    filename = 'C:/Users/ergou/Documents/PythonScripts/consultjob1_1/rf_trained_model.sav'
+    loaded_model = joblib.load(filename)
+    main()
